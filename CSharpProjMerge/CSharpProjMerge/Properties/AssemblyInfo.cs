bfe4922a50a6c67db7eba234c4a75df44514e726
Simon Mourier <smourier@users.noreply.github.com>
2025-01-04 12:09:54 +0100

    updated copyrights

--- CSharpProjMerge/CSharpProjMerge/Properties/AssemblyInfo.cs
+++ CSharpProjMerge/CSharpProjMerge/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("CSharpProjMerge")]
 [assembly: AssemblyDescription("A console utility to merge a .NET project and referenced .NET projects into one unique project.")]
 [assembly: AssemblyProduct("CSharpProjMerge")]
-[assembly: AssemblyCopyright("Copyright © 2019-2024 Simon Mourier. All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright © 2019-2025 Simon Mourier. All rights reserved.")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
